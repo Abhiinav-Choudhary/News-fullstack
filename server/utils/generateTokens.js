0c3420d695f42c7e7d26eba2efe98182d07d8103
Abhinav Chaudhary <abhinavchaudhary30993@gmail.com>
2026-05-16 14:22:58 +0530

    .

--- server/utils/generateTokens.js
+++ server/utils/generateTokens.js
@@ -15,7 +15,7 @@ const generateToken = (res, userId) => {
   res.cookie("token", token, {
     httpOnly: true,
     secure: true,
-    sameSite: "node",
+    sameSite: "none",
     maxAge: 7 * 24 * 60 * 60 * 1000
   });
 
