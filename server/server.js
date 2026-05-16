c12d25bf3890ef18e44b9b218aba320f119af15a
Abhinav Chaudhary <abhinavchaudhary30993@gmail.com>
2026-05-16 12:07:17 +0530

    .

--- server/server.js
+++ server/server.js
@@ -16,8 +16,23 @@ app.use(express.json());
 
 app.use(cookieParser());
 
+const allowedOrigins = [
+  "http://localhost:5173",
+  "https://news-fullstack-one.vercel.app/"
+];
+
 app.use(cors({
-  origin: "http://localhost:5173",
+  origin: function (origin, callback) {
+
+    // allow requests with no origin (mobile apps, postman)
+    if (!origin) return callback(null, true);
+
+    if (allowedOrigins.includes(origin)) {
+      callback(null, true);
+    } else {
+      callback(new Error("Not allowed by CORS"));
+    }
+  },
   credentials: true
 }));
 
